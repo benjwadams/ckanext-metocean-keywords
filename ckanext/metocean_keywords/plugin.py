50ac19e2f607f61f96acb3c609582f310a3dd0b2
Benjamin Adams <ben.adams@rpsgroup.com>
2022-02-25 16:49:50 -0500

    Attempt to build against assets

--- ckanext/metocean_keywords/plugin.py
+++ ckanext/metocean_keywords/plugin.py
@@ -180,7 +180,7 @@ class MetoceanKeywordsPlugin(p.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
-        toolkit.add_resource("assets", "metocean_keywords")
+        toolkit.add_resource("public", "metocean_keywords")
 
     def get_helpers(self):
         '''
