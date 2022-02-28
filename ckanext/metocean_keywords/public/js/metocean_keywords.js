bdca82b8fcf213ca61c0371d49d711453d6818a7
Benjamin Adams <ben.adams@rpsgroup.com>
2022-02-28 11:20:56 -0500

    Attempt to debug JS assets

---
+++ ckanext/metocean_keywords/public/js/metocean_keywords.js
@@ -0,0 +1,24 @@
+ckan.module("metocean_keywords-module", function ($, _) {
+  "use strict";
+  return {
+    options: {
+      debug: false,
+    },
+
+    initialize: function () {
+      $('#dataset-meta').click(function(e) {
+        e.preventDefault();
+        $(this).tab('show');
+      });
+      $('#dataset-meta-pane').show();
+
+      /* highlight any ancestor elements in the GCMD keyword hierarchy
+       * when hovered over */
+      $('ul.tree').find('a.tag').hover(
+                  function() { $(this).parentsUntil('ul.tree', 'ul').prev().
+                                          addClass('highlight-ancestor') },
+                  function() { $(this).parentsUntil('ul.tree', 'ul').prev().
+                                          removeClass('highlight-ancestor') });
+    }
+  }
+});
